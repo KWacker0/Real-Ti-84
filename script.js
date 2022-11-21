18a09461ed8c01dc2cf2b108e39746af3fe8d347
KWacker0 <keagan.walker@gmail.com>
2022-11-21 13:46:32 -0500

    added More functions

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const viewPort = document.getElementById("viewPort");
 let dispNum = 0;
+let saveNum = 0;
 
 
 //Numbers 
@@ -13,6 +14,8 @@ document.getElementById("6/l6/v").addEventListener('click', nums);
 document.getElementById("7/u/O").addEventListener('click', nums);
 document.getElementById("8/v/p").addEventListener('click', nums);
 document.getElementById("9/w/Q").addEventListener('click', nums);
+document.getElementById("(-)/ans/?").addEventListener('click', nums);
+document.getElementById("./i/:").addEventListener('click', nums);
 
 
 //Operators
@@ -23,10 +26,26 @@ document.getElementById("+/mem/opnQto").addEventListener('click', Sum);
 document.getElementById("enter/entery/solve").addEventListener('click', enter)
 document.getElementById("clear").addEventListener('click', clear)
 
+//Random Functions
+document.getElementById(")/}/l").addEventListener("click", parths);
+document.getElementById("(/{/k").addEventListener("click", parths);
+
 function nums(e) {
-    if(e.target.id >= "0/catalog/_" && e.target.id <= "9/w/Q") {
+    if(e.target.id === "./i/:") {
         if(dispNum === 0) {
             dispNum = e.target.id[0]
+            }else {
+            dispNum = dispNum + e.target.id[0];
+            }
+            viewPort.value = dispNum;
+    }
+    if(e.target.id === "(-)/ans/?") {
+        dispNum = dispNum + e.target.id[1];
+        viewPort.value = dispNum;
+    }
+    if(e.target.id >= "0/catalog/_" && e.target.id <= "9/w/Q") {
+        if(dispNum === 0) {
+        dispNum = e.target.id[0]
         }else {
         dispNum = dispNum + e.target.id[0];
         }
@@ -51,10 +70,28 @@ function Sum(e) {
 }
 function enter(e) {
     Number(dispNum)
-    let awnser = eval(dispNum)
+    saveNum = dispNum
+    let awnser = eval(dispNum);
     viewPort.value = awnser;
+    dispNum = 0;
 }
 function clear(e) {
     dispNum = 0;
+    viewPort.value = dispNum;
+}
+function parths(e) {
+     if(e.target.id === "(/{/k") {
+        if(dispNum === 0) {
+            dispNum = e.target.id[0];
+        }else {
+        dispNum = dispNum + e.target.id[0]
+        }
+    }else if(e.target.id === ")/}/l") {
+        if(dispNum === 0) {
+            viewPort.value = dispNum;
+        }else {
+        dispNum = dispNum + e.target.id[0]
+        }
+    }
     viewPort.value = dispNum;
 }
